9e2ce113d2472df169119c6f26698ad06c97cb23
BeepBoopYeetYourScoot <59568155+BeepBoopYeetYourScoot@users.noreply.github.com>
2021-01-24 02:21:46 +0300

    Добавил сервисы с моками. Алг пересчёта работает

--- core/services/dummie_manufacturers.py
+++ core/services/dummie_manufacturers.py
@@ -1,105 +1,81 @@
-player_1 = {
+from .dummie_transactions import *
+
+manufacturer_1 = {
     'nickname': 'yo',
     'user': 'test_user_1',
     'session': 'Dummie Session',
     'city': 'NF',
     'role': 'manufacturer',
     'production': {'billets_produced': 10},
-    'transactions': [],
+    'warehouse': {'billets': 0},
+    'transactions': [transaction_1, transaction_7],
     'balance': 4000,
     'is_bankrupt': False,
 }
 
-player_2 = {
+manufacturer_2 = {
     'nickname': 'sobaki',
     'user': 'test_user_2',
     'session': 'Dummie Session',
     'city': 'TT',
     'role': 'manufacturer',
     'production': {'billets_produced': 15},
-    'transactions': [],
+    'warehouse': {'billets': 0},
+    'transactions': [transaction_2],
     'balance': 4000,
     'is_bankrupt': False,
 }
 
-player_3 = {
+manufacturer_3 = {
     'nickname': 'ya',
     'user': 'test_user_3',
     'session': 'Dummie Session',
     'city': 'WS',
     'role': 'manufacturer',
     'production': {'billets_produced': 20},
-    'transactions': [],
+    'warehouse': {'billets': 0},
+    'transactions': [transaction_3],
     'balance': 4000,
     'is_bankrupt': False,
 }
 
-player_4 = {
+manufacturer_4 = {
     'nickname': 'naruto',
     'user': 'test_user_4',
     'session': 'Dummie Session',
     'city': 'AV',
     'role': 'manufacturer',
     'production': {'billets_produced': 25},
-    'transactions': [],
+    'warehouse': {'billets': 0},
+    'transactions': [transaction_4],
     'balance': 4000,
     'is_bankrupt': False,
 }
 
-player_5 = {
+manufacturer_5 = {
     'nickname': 'uzumaki',
     'user': 'test_user_5',
     'session': 'Dummie Session',
     'city': 'AD',
     'role': 'manufacturer',
     'production': {'billets_produced': 30},
-    'transactions': [],
+    'warehouse': {'billets': 0},
+    'transactions': [transaction_5],
     'balance': 4000,
     'is_bankrupt': False,
 }
 
-player_6 = {
+manufacturer_6 = {
     'nickname': 'da',
     'user': 'test_user_6',
     'session': 'Dummie Session',
     'city': 'ET',
     'role': 'manufacturer',
     'production': {'billets_produced': 0},
-    'transactions': [],
+    'warehouse': {'billets': 0},
+    'transactions': [transaction_6],
     'balance': 4000,
     'is_bankrupt': False,
 }
 
-player_7 = {
-    'nickname': 'i_kstati',
-    'user': 'test_user_7',
-    'session': 'Dummie Session',
-    'city': 'NF',
-    'role': 'broker',
-    'balance': 8000,
-    'is_bankrupt': False,
-}
-
-player_8 = {
-    'nickname': 'ya_buduschii',
-    'user': 'test_user_8',
-    'session': 'Dummie Session',
-    'city': 'TT',
-    'role': 'broker',
-    'balance': 8000,
-    'is_bankrupt': False,
-}
-
-player_9 = {
-    'nickname': 'hokage',
-    'user': 'test_user_9',
-    'session': 'Dummie Session',
-    'city': 'WS',
-    'role': 'broker',
-    'balance': 8000,
-    'is_bankrupt': False,
-}
-
-
-manufacturers = [player_1, player_2, player_3, player_4, player_5, player_6]
-brokers = [player_7, player_8, player_9]
+manufacturers = [manufacturer_1, manufacturer_2, manufacturer_3, manufacturer_4, manufacturer_5, manufacturer_6]
---
+++ core/services/dummie_transactions.py
@@ -0,0 +1,65 @@
+from .dummie_brokers import brokers
+
+transaction_1 = {
+    'broker': brokers[0],
+    'number_of_billets': 10,
+    'turn': 1,
+    'billet_price': 150,
+    'costs_transporting_single': 10,
+    'approved': True
+}
+
+transaction_2 = {
+    'broker': brokers[1],
+    'number_of_billets': 10,
+    'turn': 1,
+    'billet_price': 100,
+    'costs_transporting_single': 10,
+    'approved': True
+}
+
+transaction_3 = {
+    'broker': brokers[2],
+    'number_of_billets': 13,
+    'turn': 1,
+    'billet_price': 160,
+    'costs_transporting_single': 10,
+    'approved': True
+}
+
+transaction_4 = {
+    'broker': brokers[0],
+    'number_of_billets': 10,
+    'turn': 1,
+    'billet_price': 228,
+    'costs_transporting_single': 10,
+    'approved': True
+}
+
+transaction_5 = {
+    'broker': brokers[1],
+    'number_of_billets': 3,
+    'turn': 1,
+    'billet_price': 222,
+    'costs_transporting_single': 10,
+    'approved': True
+}
+
+transaction_6 = {
+    'broker': brokers[2],
+    'number_of_billets': 5,
+    'turn': 1,
+    'billet_price': 200,
+    'costs_transporting_single': 10,
+    'approved': True
+}
+
+transaction_7 = {
+    'broker': brokers[0],
+    'number_of_billets': 100,
+    'turn': 1,
+    'billet_price': 50,
+    'costs_transporting_single': 10,
+    'approved': True
+}
+
