e6d3793dfb4ef7fa7b02b2f7f63fb379ae42cf34
Fedor <fedorshatruk@hotmail.com>
2021-01-26 09:57:35 +0300

    created web socket router, consumers, use stories

--- core/routing.py
+++ core/routing.py
@@ -2,6 +2,10 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/game/(?P<room_name>\w+)/$', consumers.GameDetailConsumer.as_asgi()),
+    re_path(r'ws/user/(?P<session_id>\w+)/(?P<user_id>\w+)/$', consumers.UserConsumer.as_asgi()),
+    re_path(r'ws/detail/(?P<session_id>\w+)/$', consumers.SessionDetailConsumer.as_asgi()),
     re_path(r'ws/session/', consumers.GameConsumer.as_asgi()),
+
+
+
 ]
--- core/services/websocket_services/controllers.py
+++ core/services/websocket_services/controllers.py
@@ -1,24 +1,15 @@
-import json
-
 from django.db import models
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
-
-from ...models import Session, GameSetting
+from ...models import Session
 from .websocket_serializers import WSSessionSerializer
 
 
-def websocket_send_message(message_type: str = None, message: str = None, socket_obj: object = None):
-    channel_layer = get_channel_layer()
-    if message_type:
-        pass
-
-
-def get_session_controller(obj: object):
+def get_session_list_controller():
     queryset = Session.objects.exclude(status='Filled').annotate(players_quantity=models.Count('player'))
     data = WSSessionSerializer(queryset, many=True).data
-    obj.send(
-        text_data=json.dumps({
-            'data': data
-        }))
+    return data
 
+
+def get_session_detail_controller(pk):
+    queryset = Session.objects.filter(id=pk).annotate(players_quantity=models.Count('player'))
+    data = WSSessionSerializer(queryset, many=True).data
+    return data
